ba09b346f51db92f133655d527ff038e14a146c4
Austin <austinjdiehl@gmail.com>
2021-12-08 20:15:41 -0600

    edited

--- scriptHelper.js
+++ scriptHelper.js
@@ -54,6 +54,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
     let pilotStatus = document.getElementById('pilotStatus')
     let copilotStatus = document.getElementById('copilotStatus')
     
+    faulty.style.visibility = 'hidden'
     fuelStatus.innerHTML = 'Fuel level high enough for launch'
     cargoStatus.innerHTML = 'Cargo mass low enough for launch'
     launchStatus.innerHTML = 'Shuttle is Ready for Launch'
